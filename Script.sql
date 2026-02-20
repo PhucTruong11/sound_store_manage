51cb957a4b7387de8e75933b1bf711101c652c63
PhucTruong11 <truongtrongphuc584@gmail.com>
2026-02-20 09:09:06 +0700

    Fix insert into ChiTietSP

--- Script.sql
+++ Script.sql
@@ -425,7 +425,7 @@ INSERT INTO ChiTietSP (MaImei, MaPhienBan, MaPhieuNhap, TinhTrang) VALUES
 ('777888999', 'PB01', 'PN01', 'Trong kho'), 
 ('123123123', 'PB02', 'PN01', 'Trong kho'),
 ('456456456', 'PB02', 'PN01', 'Trong kho'),
-('777888999', 'PB02', 'PN01', 'Đã bán');
+('999888777', 'PB02', 'PN01', 'Đã bán');
 -- --------------------------------------------------------
 
 INSERT INTO KhuyenMai (MaKM, TenKM, DieuKienGiam, PhanTramGiam, NgayBatDau, NgayKetThuc) VALUES 
