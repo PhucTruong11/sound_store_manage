93bb45e6f6798ddda1d5c62de6f9776eaa60dc58
AizawaNagisa <vulephuocsang0212@gmail.com>
2026-03-10 11:17:57 +0700

    Thong ke

--- src/main/java/Backend/DTO/ThongKe/ThongKeDoanhThu.java
+++ src/main/java/Backend/DTO/ThongKe/ThongKeDoanhThu.java
@@ -1,5 +1,29 @@
 package Backend.DTO.ThongKe;
 
 public class ThongKeDoanhThu {
-    
+    private double doanhThu;
+    private double von;
+    private double loiNhuan;
+    private int soDonHang;
+
+    public ThongKeDoanhThu() {}
+
+    public ThongKeDoanhThu(double doanhThu, double von, double loiNhuan, int soDonHang) {
+        this.doanhThu = doanhThu;
+        this.von = von;
+        this.loiNhuan = loiNhuan;
+        this.soDonHang = soDonHang;
+    }
+
+    public double getDoanhThu() { return doanhThu; }
+    public void setDoanhThu(double doanhThu) { this.doanhThu = doanhThu; }
+
+    public double getVon() { return von; }
+    public void setVon(double von) { this.von = von; }
+
+    public double getLoiNhuan() { return loiNhuan; }
+    public void setLoiNhuan(double loiNhuan) { this.loiNhuan = loiNhuan; }
+
+    public int getSoDonHang() { return soDonHang; }
+    public void setSoDonHang(int soDonHang) { this.soDonHang = soDonHang; }
 }
--- src/main/java/Backend/DTO/ThongKe/ThongKeKhachHang.java
+++ src/main/java/Backend/DTO/ThongKe/ThongKeKhachHang.java
@@ -1,5 +1,29 @@
 package Backend.DTO.ThongKe;
 
 public class ThongKeKhachHang {
-    
+    private String maKH;
+    private String hoTen;
+    private int soDonHang;
+    private double tongChiTieu;
+
+    public ThongKeKhachHang() {}
+
+    public ThongKeKhachHang(String maKH, String hoTen, int soDonHang, double tongChiTieu) {
+        this.maKH = maKH;
+        this.hoTen = hoTen;
+        this.soDonHang = soDonHang;
+        this.tongChiTieu = tongChiTieu;
+    }
+
+    public String getMaKH() { return maKH; }
+    public void setMaKH(String maKH) { this.maKH = maKH; }
+
+    public String getHoTen() { return hoTen; }
+    public void setHoTen(String hoTen) { this.hoTen = hoTen; }
+
+    public int getSoDonHang() { return soDonHang; }
+    public void setSoDonHang(int soDonHang) { this.soDonHang = soDonHang; }
+
+    public double getTongChiTieu() { return tongChiTieu; }
+    public void setTongChiTieu(double tongChiTieu) { this.tongChiTieu = tongChiTieu; }
 }
